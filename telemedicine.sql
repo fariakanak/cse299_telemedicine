19ddd336a013a5221f46697e1c30ad2de62c1797
Md. Ahnaf Akif <56639080+AkifAhnaf@users.noreply.github.com>
2021-08-04 22:20:25 +0600

    Add files via upload

--- telemedicine.sql
+++ telemedicine.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 06, 2021 at 04:54 PM
+-- Generation Time: Aug 04, 2021 at 06:15 PM
 -- Server version: 10.4.19-MariaDB
 -- PHP Version: 8.0.7
 
@@ -50,8 +50,16 @@ CREATE TABLE `doctor` (
   `Contact_No` varchar(150) DEFAULT NULL,
   `Email` varchar(250) DEFAULT NULL,
   `Password` varchar(250) DEFAULT NULL,
-  `Spec_ID` int(250) DEFAULT NULL
+  `Spec_ID` int(250) DEFAULT NULL,
+  `fee` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `doctor`
+--
+
+INSERT INTO `doctor` (`D_ID`, `D_FirstName`, `D_LastName`, `Edu_Level`, `Bmdc_Reg_No`, `DOB`, `Contact_No`, `Email`, `Password`, `Spec_ID`, `fee`) VALUES
+(1, 'Ahnaf ', 'Akif', 'MBBS', '65778544', '0000-00-00', '0944451234', 'ahnafakif@gmail.com', '123456', 0, 500);
 
 -- --------------------------------------------------------
 
@@ -67,6 +75,13 @@ CREATE TABLE `patient` (
   `Password` varchar(150) DEFAULT NULL,
   `DOB` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `patient`
+--
+
+INSERT INTO `patient` (`P_ID`, `P_FirstName`, `P_LastName`, `Email`, `Password`, `DOB`) VALUES
+(1, 'Faria', 'Kanak', 'fr@gmail.com', '123456', '1990-10-15');
 
 -- --------------------------------------------------------
 
@@ -96,6 +111,96 @@ INSERT INTO `specialization` (`Spc_ID`, `Spc_Name`) VALUES
 (10, 'Orthopaedics'),
 (11, 'ENT'),
 (12, 'COVID-19');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_info`
+--
+
+CREATE TABLE `user_info` (
+  `U_ID` int(11) NOT NULL,
+  `email` varchar(250) NOT NULL,
+  `password` varchar(250) NOT NULL,
+  `type` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_info`
+--
+
+INSERT INTO `user_info` (`U_ID`, `email`, `password`, `type`) VALUES
+(1, 'ahnaf@gmail.com', '123456', 'p'),
+(2, 'ahnafakif@gmail.com', '123456', 'd'),
+(3, 'fr@gmail.com', '123456', 'p');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `appointment`
+--
+ALTER TABLE `appointment`
+  ADD PRIMARY KEY (`App_ID`);
+
+--
+-- Indexes for table `doctor`
+--
+ALTER TABLE `doctor`
+  ADD PRIMARY KEY (`D_ID`);
+
+--
+-- Indexes for table `patient`
+--
+ALTER TABLE `patient`
+  ADD PRIMARY KEY (`P_ID`);
+
+--
+-- Indexes for table `specialization`
+--
+ALTER TABLE `specialization`
+  ADD PRIMARY KEY (`Spc_ID`);
+
+--
+-- Indexes for table `user_info`
+--
+ALTER TABLE `user_info`
+  ADD PRIMARY KEY (`U_ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `appointment`
+--
+ALTER TABLE `appointment`
+  MODIFY `App_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `doctor`
+--
+ALTER TABLE `doctor`
+  MODIFY `D_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `patient`
+--
+ALTER TABLE `patient`
+  MODIFY `P_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `specialization`
+--
+ALTER TABLE `specialization`
+  MODIFY `Spc_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `user_info`
+--
+ALTER TABLE `user_info`
+  MODIFY `U_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
